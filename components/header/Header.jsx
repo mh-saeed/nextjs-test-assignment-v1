9ef14ccdc857bd405819d0d1dee515d4c700168f
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-05-05 17:05:01 +0500

    header completed

---
+++ components/header/Header.jsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import styles from "./header.module.css";
+import { Typography, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import React from "react";
+
+const { Text } = Typography;
+
+const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href={"/"} className={styles.Link}>
+          <Text className={styles.logo}>AAA</Text>
+        </Link>
+
+        <span className={styles.button}>
+          <Link href={"/"} className={styles.Link}>
+            <Text className={styles.navItem}>Feedback</Text>
+          </Link>
+
+          <Link href={"/"} className={styles.Link}>
+            <Text className={styles.navItem}>Support</Text>
+          </Link>
+
+          <Avatar
+            style={{ backgroundColor: "#87d068" }}
+            icon={<UserOutlined />}
+          />
+        </span>
+      </header>
+      <div className={styles.line}></div>
+    </>
+  );
+};
+
+export default Header;
